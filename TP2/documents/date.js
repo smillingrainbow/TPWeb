c612c9af8285cd7bbd032c98a89a4c4d327722b7
Natacha <nachoumm@hotmail.fr>
2013-06-05 10:32:46 +0200

    javasript + modif classe du footer

--- TP2/documents/date.js
+++ TP2/documents/date.js
@@ -1,14 +1,17 @@
 
 
 function affichageDate(){
-	var jour = getDate();
-	var annee = getFullYear();
-	var month = getMonth();
-	var heure = getHours();
-	var minute = getMinutes();
+	var fdate = new Date();
+	var jour = fdate.getDate();
+	var annee = fdate.getFullYear();
+	var mois = fdate.getMonth();
+	var heure = fdate.getHours();
+	var minute = fdate.getMinutes();
+	var seconde = fdate.getSeconds();
 	var date = jour+"/"+mois+"/"+annee;
-	var hour = heure+":"+minute;
+	var hour = heure+":"+minute+":"+seconde;
 	var mess = "Nous sommes le "+date+" il est"+hour+".<br>";
-	document.getElementById("date").innerHTML=mess;
+	document.getElementById('date').innerHTML=mess;
+	setTimeout(affichageDate,1000);
 }
 affichageDate();
