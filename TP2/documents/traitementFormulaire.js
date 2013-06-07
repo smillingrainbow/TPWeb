e5ab1aa855d516f33f0dd9f1fd082b4c1e266db4
Natacha <nachoumm@hotmail.fr>
2013-06-07 08:41:04 +0200

    ajout d'alert pour le formulaire

--- TP2/documents/traitementFormulaire.js
+++ TP2/documents/traitementFormulaire.js
@@ -4,16 +4,20 @@ function validateform() {
 	var prenom = document.forms["myForm"]["prenom"].value;
 	var email = document.forms["myForm"]["email"].value; 
 	var mess ="";
-	if (nom==null || nom=="" || prenom==null || prenom=="" || email==null || email==""){
-		//si l’utilisateur n’a rien saisi, affichage d’une alerte 
+	if (nom!=null && nom!="" && prenom!=null && prenom!="" && email!=null && email!=""){
+		alert("Votre nom est "+nom+"Votre prenom est "+prenom+"Votre email est "+email)
+		return true;		
+	}
+	else{
+	//si l’utilisateur n’a rien saisi, affichage d’une alerte
 		if (nom==null || nom=="") {
-			mess = "votre nom"
+			mess +="votre nom"
 		} 
-		else if(prenom==null || prenom==""){
-			mess+= " votre prenom"
+		if(prenom==null || prenom==""){
+			mess+=" votre prenom"
 		}
-		else if(email==null || email==""){
-			mess += " votre email"
+		if(email==null || email==""){
+			mess+=" votre email"
 		};
 		alert(" Vous devez saisir "+mess+" Votre message est vide"); 
 		return false; 
