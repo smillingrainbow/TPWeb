d70e5c94a6d6818df8687acfb1d7306748b8f645
Natacha <nachoumm@hotmail.fr>
2013-06-11 12:34:00 +0200

    connexion pour la page de formulaire de recette

--- projet/Pages/formconnexion.php
+++ projet/Pages/formconnexion.php
@@ -1,15 +1,42 @@
 <?php
-$server = 'locahost';
-$user = 'root';
-$password ='';
 
-if($link = mysqli_connect( $server,$user,$password)){
-	if ($id_BD = mysqli_select_db($link, $database)) {
-		echo "succes de la connexion";
-	} else {
-		die("echec de la connexion $database");
+	$host = '127.0.0.1';
+	$user = 'root';
+	$password ='';
+	$database = 'projetWeb';
+	$login = $_POST['login'];
+	$pwd = $_POST['pwd'];
+	if($link = mysqli_connect($host,$user,$password)){
+		echo "succes de la connexion 1";
+		if ($id_BD = mysqli_select_db($link, $database)) {
+			echo "succes de la connexion";
+			$query = 'SELECT * FROM Connexion';
+			if ($result = mysqli_query($link,$query)){
+					while ($row=mysqli_fetch_assoc($result)) {
+					$log = $row['login'];
+					$pass =  $row['pwd'];
+					echo("$log $pass");
+					$complog = strcmp($log, $login);
+					$comppwd = strcmp($pass, $pwd);
+					$test = ($comppwd == 0);
+					echo("$test");
+					if (($complog == 0) & ($comppwd == 0)) {
+						echo "comparatif";
+						header('Location: formulairerecette.php');
+					}
+				}
+				mysqli_free_result($result);
+			}
+		} else {
+			die("echec de la connexion $database");
+		}
+		mysqli_close($link);
+	}else {
+		die("Echec de la connexion au server $host");
 	}
 
-}
+
+
+
 
 ?>
--- projet/Pages/index.php
+++ projet/Pages/index.php
@@ -17,11 +17,13 @@
 				Page d'accueil
 			</p>
 		<section>
-			<form method="post">
+			
+			<form method="post" action="formconnexion.php" name="formconnexion">
 				<label for="login"> Login : </label><input type="text" id="login" name="login" value="login" placeholder="login">
 				<label for="pwd"> Password : </label><input type="password" id="pwd" name="pwd" value="passeword" placeholder="pwd">
 				<input type="submit" name="valider" value="Envoyer"><br>
 			</form>
+
 			<a href="formulairerecette.php" title="formulairerecette"> Formulaire </a>
 		</section>	
 		<?php include 'navigation.inc'; ?>	
